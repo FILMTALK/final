b53d860a52f8eb3dee111f9277151ae9ffa1f297
iniwo <inigovinas@hotmail.com>
2015-01-23 11:46:45 +0100

    margin canvas

--- views/profile.php
+++ views/profile.php
@@ -76,7 +76,6 @@ if(!(isset($_SESSION['id_usuario']) && $_SESSION['id_usuario']!='')){
                     }
 
                 ?>
-
                 <div class="profile">
 
                     <div class="editarDatos">
