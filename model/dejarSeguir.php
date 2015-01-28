27298d3c081575789085888ca99a4b3b6fe105ea
amaiafdez <afernandezvi13dw@ikzubirimanteo.com>
2015-01-28 14:17:28 +0100

    Update dejarSeguir.php

--- model/dejarSeguir.php
+++ model/dejarSeguir.php
@@ -6,13 +6,13 @@ include_once("../config/database.php");
 $exit;
 
 $titulo=$_POST['titulo'];
-
+$id_usuario=$_POST['id_usuario'];
 
 
 try{
 	$collection=$bd->sigue_peli;
 
-	$eliminar=$collection->remove(array( 'titulo' => "$titulo"));
+	$eliminar=$collection->remove(array( 'titulo' => "$titulo", 'id_usuario' => "$id_usuario"));
 
 	// Devuelve el objeto JSON
 	$exit=true;
